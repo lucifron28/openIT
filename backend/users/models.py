55f61db7ad7e25348591a621bcb4f5cfb02fbe47
ron <lucifron28@gmail.com>
2025-07-12 14:29:46 +0800

    add more attributes

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-
+    role = models.CharField(max_length=20)
+    current_streak = models.IntegerField(default=0)
+    longest_streak = models.IntegerField(default=0)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    team_id = models.IntegerField(null=True, blank=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'email', ]
 
